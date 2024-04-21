6c8a061a648c02d25f9c2100e47fad7cb8b8c8b4
pistii <pilisi.istvan@gmail.com>
2024-04-21 18:31:18 +0200

    Generic class for mock database

---
+++ Helpers/DBContextMock.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace KozossegiAPI.UnitTests.Helpers
+{
+    public class DbContextMock
+    {
+        public static Mock<DbSet<TData>> GetMock<TData>(List<TData> lstData) where TData : class
+        {
+            IQueryable<TData> lstDataQueryable = lstData.AsQueryable();
+            Mock<DbSet<TData>> dbSetMock = new Mock<DbSet<TData>>();
+
+            dbSetMock.As<IQueryable<TData>>().Setup(s => s.Provider).Returns(lstDataQueryable.Provider);
+            dbSetMock.As<IQueryable<TData>>().Setup(s => s.Expression).Returns(lstDataQueryable.Expression);
+            dbSetMock.As<IQueryable<TData>>().Setup(s => s.ElementType).Returns(lstDataQueryable.ElementType);
+            dbSetMock.As<IQueryable<TData>>().Setup(s => s.GetEnumerator()).Returns(() => lstDataQueryable.GetEnumerator());
+            dbSetMock.Setup(x => x.Add(It.IsAny<TData>())).Callback<TData>(lstData.Add);
+            dbSetMock.Setup(x => x.AddRange(It.IsAny<IEnumerable<TData>>())).Callback<IEnumerable<TData>>(lstData.AddRange);
+            dbSetMock.Setup(x => x.Remove(It.IsAny<TData>())).Callback<TData>(t => lstData.Remove(t));
+            dbSetMock.Setup(x => x.RemoveRange(It.IsAny<IEnumerable<TData>>())).Callback<IEnumerable<TData>>(ts =>
+            {
+                foreach (var t in ts) { lstData.Remove(t); }
+            });
+
+            return dbSetMock;
+        }
+    }
+}
