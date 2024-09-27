ed21938a39759f35416dbe298ee5f4c2fcc47f79
pistii <pilisi.istvan@gmail.com>
2024-09-27 18:02:59 +0200

    Add Setting repo tests

---
+++ Helpers/TestData/SettingData.cs
@@ -0,0 +1,27 @@
+﻿
+using KozossegiAPI.Models;
+
+namespace KozossegiAPI.UnitTests.Helpers.TestData
+{
+    public static class SettingData
+    {
+        public static IQueryable<Settings> GetSettings()
+        {
+            var i = 1;
+            List<Settings> settings = new List<Settings>();
+            while (i < 10)
+            {
+                var setting = new Settings()
+                {
+                    FK_UserId = i,
+                    PK_Id = i,
+                    NextReminder = DateTime.Now
+                };
+                settings.Add(setting);
+                i++;
+            }
+
+            return settings.AsQueryable();            
+        }
+    }
+}
---
+++ Repo/SettingRepositoryTests.cs
@@ -0,0 +1,66 @@
+﻿using KozossegiAPI.Controllers.Cloud;
+using KozossegiAPI.Data;
+using KozossegiAPI.DTOs;
+using KozossegiAPI.Interfaces;
+using KozossegiAPI.Models;
+using KozossegiAPI.Repo;
+using KozossegiAPI.UnitTests.Helpers.TestData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KozossegiAPI.UnitTests.Repo
+{
+    [TestFixture]
+    internal class SettingRepositoryTests
+    {
+        private ServiceProvider _serviceProvider;
+        private ISettingRepository _settingRepository;
+
+        private DBContext _dbContext = new();
+
+        [SetUp]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+            services.AddDbContext<DBContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
+            services.AddScoped<ISettingRepository, SettingRepository>();
+            _serviceProvider = services.BuildServiceProvider();
+
+            var scope = _serviceProvider.CreateScope();
+
+            var scopedServices = scope.ServiceProvider;
+            _settingRepository = scopedServices.GetRequiredService<ISettingRepository>();
+            _dbContext = scopedServices.GetRequiredService<DBContext>();
+
+            var personal = PersonalData.GetUsers();
+            var setting = SettingData.GetSettings();
+            var user = UserData.GetUsers();
+            var studies = StudyData.GetStudies();
+            _dbContext.AddRange(personal);
+            _dbContext.AddRange(setting);
+            _dbContext.AddRange(user);
+            _dbContext.AddRange(studies);
+            _dbContext.SaveChanges();
+        }
+
+        
+        [TearDown]
+        public void Cleanup()
+        {
+            
+            var dbContext = _serviceProvider.GetService<DBContext>();
+            if (dbContext != null)
+                dbContext.Database.EnsureDeleted();
+        }
+
+
+        [Test]
+        public async Task GetSettings_ReturnsSettins()
+        {
+            var result = await _settingRepository.GetSettings(1);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
